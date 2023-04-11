1261a54ecb9842484b87a698453e455fd72ad352
PedroHPrieto <pprieto677@gmail.com>
2023-04-11 16:17:45 -0300

    conversor-moeda

--- js/script.js
+++ js/script.js
@@ -1,125 +1,40 @@
-var v1;
-var v2;
-
-
-function medidaUm(brl) {
-    var btn1 = document.getElementById('btn1');
-    
-    switch (brl) {
-        case 'brl':
-            v1 = 'brl';
-            btn1.textContent = 'brl ';
+var moeda, cotDolar, cotEuro, cotLibra, ResultDolar, ResultEuro, ResultLibra;
+function cotar(c) {
+    switch (c) {
+        case 'BR':
+            moeda = document.getElementById("reais").value;
             break;
 
-        case 'euro':
-            v1 = 'euro';
-            btn1.textContent = 'euro';
-            break;
-        case 'libra':
-            v1 = 'libra';
-            btn1.textContent = 'libra';
-            case 'dolar':
-                v1 = 'dolar';
-                btn1.textContent = 'dolar';
-    }
-}
-
-function medidaDois(m2) {
-    var btn2 = document.getElementById('btn2');
-    switch (m2) {
-        case 'ml':
-            v2 = 'ml';
-            btn2.textContent = 'Metros';
+        case 'US':
+            cotDolar = document.getElementById("Dolar").value;
             break;
 
-        case 'km':
-            v2 = 'km';
-            btn2.textContent = 'Quilômetros';
-            break;
-        case 'pes':
-            v2 = 'pes';
-            btn2.textContent = 'Pés';
-    }
-}
-
-function medidaDois(m3) {
-    var btn2 = document.getElementById('btn2');
-    switch (m2) {
-        case 'ml':
-            v2 = 'ml';
-            btn2.textContent = 'Metros';
+        case 'EU':
+            cotEuro = document.getElementById("Euro").value;
             break;
 
-        case 'km':
-            v2 = 'km';
-            btn2.textContent = 'Quilômetros';
+        case 'LB':
+            cotLibra = document.getElementById("Libra").value;
             break;
-        case 'pes':
-            v2 = 'pes';
-            btn2.textContent = 'Pés';
-    }
-}
-
-function medidaDois(m4) {
-    var btn2 = document.getElementById('btn2');
-    switch (m2) {
-        case 'ml':
-            v2 = 'ml';
-            btn2.textContent = 'Metros';
-            break;
-
-        case 'km':
-            v2 = 'km';
-            btn2.textContent = 'Quilômetros';
-            break;
-        case 'pes':
-            v2 = 'pes';
-            btn2.textContent = 'Pés';
-    }
-}
-function Calcular() {
-
-    var valor = parseFloat(document.getElementById('valor').value);
-    if (v1 === 'brl') {
-        if (v2 === 'ml') {
-            valor = valor;
-        }
-
-        else if (v2 === 'km') {
-            valor /= 1000;
-        }
-
-        else {
-            valor *= 3.281;
-        }
     }
 
-    else if (v1 === 'brl') {
-        if (v2 === 'ml') {
-            valor *= 1000;
+    if (moeda != null) {
+        if (cotDolar != null) {
+            ResultDolar = moeda / cotDolar;
+            document.getElementById("Conv1").value = ResultDolar.toFixed(2);
+
+        }
+        if (cotEuro != null) {
+            ResultEuro = moeda / cotEuro;
+            document.getElementById("Conv2").value = ResultEuro.toFixed(2);
+        }
+        if (cotLibra != null) {
+            ResultLibra = moeda / cotLibra;
+            document.getElementById("Conv3").value = ResultLibra.toFixed(2);
         }
 
-        else if (v2 === 'km') {
-            valor = valor;
-        }
 
-        else {
-            valor *= 1381;
-        }
     }
 
-    else if (v1 === 'brl') {
-        if (v2 === 'ml') {
-            valor /= 1.381;
-        }
 
-        else if (v2 === 'km') {
-            valor *= 1381;
-        }
-
-        else {
-            valor = valor;
-        }
-    }
-    inputResultado.value = valor;
 }
